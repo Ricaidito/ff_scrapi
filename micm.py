33a6bfc6949d43338526b38cc8b05c577e01b587
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2023-05-29 18:58:02 -0400

    Added section support

--- micm.py
+++ micm.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from pprint import pprint
 
 
@@ -10,11 +9,13 @@ URL = "https://preciosjustos.micm.gob.do/"
 WAIT_TIME = 3
 
 
+# Save the html to a file
 def save_to_html(html_content, file_name):
     with open(file_name, "w") as f:
         f.write(html_content)
 
 
+# Get the html of the basic basket
 def get_basic_basket_html(url: str) -> str:
     driver = webdriver.Chrome()
 
@@ -25,12 +26,17 @@ def get_basic_basket_html(url: str) -> str:
 
     driver.quit()
 
-    save_to_html(html_doc, "basic_basket.html")
-
     return html_doc
 
 
-def get_meat_html(url: str) -> str:
+# Save the html of the basic basket
+def save_basic_basket_html(url: str, file_name: str):
+    html_doc = get_basic_basket_html(url)
+    save_to_html(html_doc, file_name)
+
+
+# Get the html of a section
+def get_section_html(url: str, category_name: str) -> str:
     driver = webdriver.Chrome()
 
     driver.get(url)
@@ -38,28 +44,50 @@ def get_meat_html(url: str) -> str:
 
     # Switch to meat category
     meat_category = driver.find_element(
-        By.CSS_SELECTOR, "li.nav-item[data-category='Carnes'] a.nav-link"
+        By.CSS_SELECTOR, f"li.nav-item[data-category='{category_name}'] a.nav-link"
     )
     meat_category.click()
 
     # Wait for the click to be done
     driver.implicitly_wait(WAIT_TIME)
 
-    # Find the "Mas Productos" button and click it
-    mas_productos_button = driver.find_element(
-        By.XPATH, "//*[contains(text(), 'Mas Productos')]"
-    )
+    while True:
+        try:
+            # Wait for the products to load
+            driver.implicitly_wait(WAIT_TIME)
 
-    # Click the button using javascript
-    driver.execute_script("arguments[0].click();", mas_productos_button)
+            # Find the "Mas Productos" button and click it
+            mas_productos_button = driver.find_element(
+                By.XPATH, "//*[contains(text(), 'Mas Productos')]"
+            )
+            # Click the button using javascript
+            driver.execute_script("arguments[0].click();", mas_productos_button)
 
+            # If the button to get more products is hidden, stop the loop
+            if not mas_productos_button.is_displayed():
+                break
+
+        # If any of the elements are not found, stop the loop
+        except NoSuchElementException:
+            break
+
+    # Wait for the products to load
     driver.implicitly_wait(WAIT_TIME)
 
-    save_to_html(driver.page_source, "meat.html")
+    html_content = driver.page_source
 
-    return driver.page_source
+    driver.quit()
+
+    return html_content
 
 
+# Save the html of a section
+def save_get_section_html(url: str, category_name: str, file_name: str):
+    html_content = get_section_html(url, category_name)
+    save_to_html(html_content, file_name)
+
+
+# Extract the products and prices from the basic basket
 def extract_basic_basket(html_content: str) -> list[dict[str, str]]:
     basic_basket = []
     soup = BeautifulSoup(html_content, "html.parser")
@@ -79,12 +107,35 @@ def extract_basic_basket(html_content: str) -> list[dict[str, str]]:
     return basic_basket
 
 
-def extract_meat_prices(html_content: str) -> list[dict[str, str]]:
-    pass
+# Extract the products and prices from a section
+def extract_section_prices(html_content: str) -> list[dict[str, str]]:
+    items = []
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    products = soup.find_all("div", class_="product-card")
+
+    for product in products:
+        name = product.find("span", class_="productTitle").text.strip()
+        price = product.find("strong", class_="productPrice").text.strip()
+        items.append(
+            {
+                "productName": name,
+                "productPrice": price,
+            }
+        )
+
+    return items
 
 
+# Driver code
 def main():
-    x = get_meat_html(URL)
+    # save_get_section_html(URL, "Carnes", "carnes.html")
+
+    # products = extract_basic_basket(get_basic_basket_html(URL))
+    # pprint(products)
+
+    products = extract_section_prices(get_section_html(URL, "Carnes"))
+    pprint(products)
 
 
 if __name__ == "__main__":
