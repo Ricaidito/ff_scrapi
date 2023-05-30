d26cafd9a96b48602242ba75d7eb710f971d8522
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2023-05-30 16:27:38 -0400

    Added class and driver code for Sirena

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scraping.micm import MICMP, MICMPCategory
+from scraping.sirena import Sirena, SirenaCategory
 
 
 def main():
@@ -8,6 +9,14 @@ def main():
 
     MICMP.print_products(basket)
     MICMP.print_products(meat)
+
+    sirena = Sirena(SirenaCategory.DELI)
+    deli = sirena.get_products()
+    sirena.switch_category(SirenaCategory.LISTOS_PARA_COMER)
+    ready_to_eat = sirena.get_products()
+
+    Sirena.print_products(deli)
+    Sirena.print_products(ready_to_eat)
 
 
 if __name__ == "__main__":
--- scraping/sirena.py
+++ scraping/sirena.py
@@ -1,41 +1,10 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 from enum import Enum
-
-
-# Test code
-category = "carnes"
-url = f"https://sirena.do/products/category/{category}?page=1&limit=0&sort=1"
-wait_time = 2
-
-driver_options = ChromeOptions()
-driver_options.add_argument("--headless=new")
-
-driver = webdriver.Chrome(options=driver_options)
-
-driver.get(url)
-
-driver.implicitly_wait(wait_time)
-
-html_doc = driver.page_source
-
-driver.quit()
-
-soup = BeautifulSoup(html_doc, "html.parser")
-
-product_divs = soup.find_all("div", class_="item-product-info")
-print("Found", len(product_divs), "products")
-
-for product_div in product_divs:
-    title = product_div.find("p", class_="item-product-title").text.strip()
-    price = product_div.find("p", class_="item-product-price").strong.text.strip()
-
-    print("Title:", title)
-    print("Price:", price)
-    print("---")
 
 
 class SirenaCategory(Enum):
@@ -52,6 +21,73 @@ class SirenaCategory(Enum):
 
 
 class Sirena:
-    def __init__(self, category: SirenaCategory, wait_time_seconds: int = 2):
-        self.__wait_time = 2
-        self.__base_url = "https://sirena.do/products/category/"
+    def __init__(self, category: SirenaCategory, wait_time_seconds: int = 5):
+        self.__wait_time = wait_time_seconds
+        self.__category = category
+        self.__base_url = f"https://sirena.do/products/category/{self.__category.value}?page=1&limit=0&sort=1"
+
+    def __extract_products(self) -> str:
+        driver_options = ChromeOptions()
+        driver_options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=driver_options)
+
+        driver.get(self.__base_url)
+
+        WebDriverWait(driver, self.__wait_time).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "div.item-product-info"))
+        )
+        driver.implicitly_wait(self.__wait_time)
+
+        html_doc = driver.page_source
+
+        driver.quit()
+
+        return html_doc
+
+    def __get_products(self, html_content: str) -> list[dict[str, str]]:
+        items = []
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        products = soup.find_all("div", class_="item-product-info")
+
+        for product in products:
+            name = product.find("p", class_="item-product-title").text.strip()
+            price = product.find("p", class_="item-product-price").strong.text.strip()
+            items.append(
+                {
+                    "productName": name,
+                    "productPrice": price,
+                    "category": self.__category.value.lower(),
+                }
+            )
+        return items
+
+    def switch_category(self, category: SirenaCategory):
+        self.__category = category
+        self.__base_url = f"https://sirena.do/products/category/{self.__category.value}?page=1&limit=0&sort=1"
+
+    def get_products(self) -> list[dict[str, str]]:
+        html = self.__extract_products()
+        products = self.__get_products(html)
+        return products
+
+    def print_products(products: list[dict[str, str]]):
+        for product in products:
+            print(
+                f"{product['productName']}: {product['productPrice']} - {product['category']}"
+            )
+        print("\n")
+
+
+def main():
+    sirena = Sirena(SirenaCategory.CARNES)
+    meats = sirena.get_products()
+    sirena.switch_category(SirenaCategory.DELI)
+    deli = sirena.get_products()
+
+    Sirena.print_products(meats)
+    Sirena.print_products(deli)
+
+
+if __name__ == "__main__":
+    main()
