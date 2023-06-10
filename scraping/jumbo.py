2f0ee775af0bc9d32222420a829cfebbc843f0a3
Carlos Collado <carlose0808@hotmail.com>
2023-06-09 22:18:20 -0400

    Fix Web Scraping

--- scraping/jumbo.py
+++ scraping/jumbo.py
@@ -1,45 +1,112 @@
+# from scraping.categories.category import SirenaCategory
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import time
 
-url = "https://jumbo.com.do/supermercado/carnes-pescados-y-mariscos/carnes.html?p=1"
+url = "https://jumbo.com.do/supermercado/carnes-pescados-y-mariscos/carnes.html?product_list_limit=all"
 
 # Initialize the Chrome driver
-driver = webdriver.Chrome('path_to_your_chromedriver')
+driver_options = ChromeOptions()
+driver_options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=driver_options)
 
 # Get the web page
 driver.get(url)
 
-while True:
-    # Wait for the page to load
-    time.sleep(3)
+# Get the HTML content of the web page
+html = driver.page_source
 
-    # Get the HTML content of the web page
-    html = driver.page_source
+# Parse the HTML with Beautiful Soup
+soup = BeautifulSoup(html, 'html.parser')
 
-    # Parse the HTML with Beautiful Soup
-    soup = BeautifulSoup(html, 'html.parser')
+# Find all product containers
+product_containers = soup.find_all('div', class_='product-item-details')
 
-    # Find all product containers
-    product_containers = soup.find_all('div', class_='product-item-details')
+for product in product_containers:
+    # Extract product name
+    product_name = product.find('a', class_='product-item-tile__link').text.strip()
 
-    for product in product_containers:
-        # Extract product name
-        product_name = product.find('a', class_='product-item-tile__link').text.strip()
+    # Extract product price
+    product_price = product.find('span', class_='product-item-tile__price-current').text.strip()
 
-        # Extract product price
-        product_price = product.find('span', class_='product-item-tile__price-current').text.strip()
-
-        print(f'Product: {product_name}, Price: {product_price}')
-
-    # Try to click the "next page" button
-    try:
-        next_button = driver.find_element_by_css_selector('a[title="Siguiente"]')
-        next_button.click()
-    except Exception as e:
-        # If the "next page" button is not found, we're probably on the last page
-        print("End of pages")
-        break
+    print(f'Product: {product_name}, Price: {product_price}')
 
 # Close the driver
 driver.quit()
+
+
+
+# class Jumbo:
+#     def __init__(self, wait_time_seconds: int = 20):
+#         self.__wait_time = wait_time_seconds
+#         # self.__category = category
+#         self.__base_url = f"https://jumbo.com.do/supermercado/carnes-pescados-y-mariscos/carnes.html?product_list_limit=all"
+
+#     def __extract_products(self) -> str:
+#         driver_options = ChromeOptions()
+#         driver_options.add_argument("--headless=new")
+#         driver = webdriver.Chrome(options=driver_options)
+
+#         driver.get(self.__base_url)
+
+#         WebDriverWait(driver, self.__wait_time).until(
+#             EC.visibility_of_element_located((By.CSS_SELECTOR, "div.products-grid"))
+#         )
+#         driver.implicitly_wait(self.__wait_time)
+
+#         html_doc = driver.page_source
+
+#         driver.quit()
+
+#         return html_doc
+
+#     def __get_products(self, html_content: str) -> list[dict[str, str]]:
+#         items = []
+#         soup = BeautifulSoup(html_content, "html.parser")
+
+#         products = soup.find_all("div", class_="product-item-details")
+
+#         for product in products:
+#             name = product.find("a", class_="product-item-tile__link").text.strip()
+#             price = product.find("span", class_="product-item-tile__price-current").strong.text.strip()
+#             items.append(
+#                 {
+#                     "productName": name,
+#                     "productPrice": price,
+#                     "category": self.__category.value.lower(),
+#                 }
+#             )
+#         return items
+
+#     def switch_category(self):
+#         # self.__category = category
+#         self.__base_url = f"https://sirena.do/products/category/{self.__category.value}?page=1&limit=0&sort=1"
+
+#     def get_products(self) -> list[dict[str, str]]:
+#         html = self.__extract_products()
+#         products = self.__get_products(html)
+#         return products
+
+#     def print_products(products: list[dict[str, str]]):
+#         for product in products:
+#             print(
+#                 f"{product['productName']}: {product['productPrice']} - {product['category']}"
+#             )
+#         print("\n")
+
+
+# def main():
+#     jumbo = Jumbo()
+#     meats = jumbo.get_products()
+#     # sirena.switch_category(SirenaCategory.DELI)
+#     # deli = sirena.get_products()
+
+#     Jumbo.print_products(meats)
+#     # Sirena.print_products(deli)
+
+
+# if __name__ == "__main__":
+#     main()
