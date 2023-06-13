5c160329292f3c1a0ca48b0ad52418088c51a6bd
Carlos Collado <carlose0808@hotmail.com>
2023-06-13 11:52:58 -0400

    Fixing

--- scraping/nacional2.py
+++ scraping/nacional2.py
@@ -8,54 +8,40 @@ import time
 
 # Initialize the Chrome driver
 driver_options = ChromeOptions()
-# driver_options.add_argument("--headless=new")
+driver_options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=driver_options)
 
+
+page_count = 1
+
 # Open the webpage
-driver.get('https://supermercadosnacional.com/carnes-pescados-y-mariscos/carnes')
+driver.get(f'https://supermercadosnacional.com/carnes-pescados-y-mariscos/carnes/res?p={page_count}')
 
 # Wait for the page to load
-time.sleep(5)
+time.sleep(7)
 
-# Scroll down to the bottom of the page until no more new products are loaded
+# Get the page source
+page_source = driver.page_source
+
+first_page_source = driver.page_source
+
 while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    # Wait to load page
-    time.sleep(15)
+    page_count += 1
 
-    # # Check if the "Load More" button exists
-    # try:
-    #     # Find the 'a' tag within the 'div' tag with a specific class
-    #     load_more_button = driver.find_element_by_xpath("//div[@class='ias-trigger ias-trigger-next']//a")
-    #     # Click the button
-    #     load_more_button.click()
+    driver.get(f'https://supermercadosnacional.com/carnes-pescados-y-mariscos/carnes/res?p={page_count}')
 
-    #     time.sleep(6)
-    # except Exception as e:
-    #     print(e)
-    #     break
+    # Wait for the page to load
+    time.sleep(7)
 
-# Once all products are loaded, you can use Beautiful Soup to parse the page HTML
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+    new_page_source = driver.page_source
 
-# print(soup)
-
-# Find all product elements (replace 'product_css_selector' with the actual CSS selector for the products)
-products = soup.find_all('li', class_='product-item')
-
-# Loop through the products and extract the information
-for product in products:
-    # Extract the product name (replace 'name_css_selector' with the actual CSS selector for the product name)
-    name = product.find('a', class_='product-item-link').text.strip()
-
-    # Extract the product price (replace 'price_css_selector' with the actual CSS selector for the product price)
-    price = product.find('span', class_='price').text.strip()
-
-    print(f'Product Name: {name}, Price: {price}')
+    if first_page_source == new_page_source:
+        break
+    else:
+        page_source += new_page_source
 
 
+print(page_source)
 
-# Close the driver
 driver.quit()
