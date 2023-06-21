df0b9935457171400db7e53276cb34eda77f2a09
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2023-06-20 22:37:18 -0400

    Added description metadata

--- main.py
+++ main.py
@@ -12,18 +12,20 @@ app = FastAPI()
 product_collection = DBService().get_collection("products")
 
 
-@app.get("/")
-def read_root():
+@app.get("/", description="Root of the API, returns the docs url")
+def api_root():
     return {"docs": f"http://localhost:{PORT}/docs"}
 
 
-@app.get("/products", response_model=list[Product])
+@app.get("/products", response_model=list[Product], description="Get all products")
 def get_products():
     products = serialize_products(product_collection.find().limit(10))
     return products
 
 
-@app.get("/product/{product_id}", response_model=Product)
+@app.get(
+    "/product/{product_id}", response_model=Product, description="Get a product by id"
+)
 def get_product(product_id: str):
     product = serialize_product(
         product_collection.find_one({"_id": ObjectId(product_id)})
@@ -31,7 +33,9 @@ def get_product(product_id: str):
     return product
 
 
-@app.get("/basic-basket", response_model=BasicBasket)
+@app.get(
+    "/basic-basket", response_model=BasicBasket, description="Get the basic basket"
+)
 def get_basic_basket():
     basic_basket_products = serialize_products(
         product_collection.find({"category": "basic_basket"})
