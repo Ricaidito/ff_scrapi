916812e638c1e61a37c06587f5f3c842f945ebf6
Carlos Collado <carlose0808@hotmail.com>
2023-07-05 00:36:29 -0400

    Added categories in Nacional

--- scraping/categories/category.py
+++ scraping/categories/category.py
@@ -40,4 +40,12 @@ class JumboCategory(Enum):
 
 # TODO: Make this class and implement the categories in the nacional.py file
 class NacionalCategory(Enum):
-    pass
+    CARNES = "carnes-pescados-y-mariscos/carnes"
+    PESCADOS = "carnes-pescados-y-mariscos/pescados-y-mariscos"
+    LACTEOS = "lacteos-y-huevos/leches"
+    QUESOS = "quesos-y-embutidos/quesos"
+    HUEVOS = "lacteos-y-huevos/huevos"
+    FRUTAS = "frutas-y-vegetales/frutas"
+    HORTALIZAS = "frutas-y-vegetales/vegetales-y-hortalizas"
+    VIVERES = "frutas-y-vegetales/viveres"
+    EMBUTIDOS = "quesos-y-embutidos/charcuteria-y-embutidos"
